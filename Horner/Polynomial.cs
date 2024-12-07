5a1548cb2fb8556ac73057889a389fccc1e4bd33
Ilia Granchulov <iliya0620@outlook.com>
2024-12-07 21:13:51 +0200

    Add project files.

---
+++ Horner/Polynomial.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Security.Cryptography;
+
+public class Polynomial
+{
+    private int degree;
+    private double[] Coef;
+    public int Degree
+    {
+        get => degree;
+        set
+        {
+            degree = value;
+            Coef = new double[degree + 1];
+        }
+    }
+    public double[] coef => Coef;
+    public void display()
+    {
+        for (int i = 0, j = Degree; i < Degree; i++, j--)
+        {
+            if (i != Degree - 1)
+            {
+                if (coef[i] < 0)
+                {
+                    if (coef[i] == -1)
+                    {
+                        Console.Write("-x^{0}", j);
+                    }
+                    else
+                    {
+                        Console.Write("-{0}x^{1}", Math.Abs(coef[i]), j);
+                    }
+
+                }
+                else if (coef[i] == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (i == 0 && coef[i] == 1)
+                    {
+                        Console.Write("x^{0}",j);
+                    }
+                    else if (i == 0)
+                    {
+                        Console.Write("{0}x^{1}", coef[i], j);
+                    }
+                    else
+                    {
+                        Console.Write("+{0}x^{1}", coef[i], j);
+                    }
+                }
+            }
+            else if (i == Degree - 1)
+            {
+                if (coef[i] < 0)
+                {
+                    if (coef[i] == -1)
+                    {
+                        Console.Write("-x");
+                    }
+                    else
+                    {
+                        Console.Write("-{0}x", Math.Abs(coef[i]));
+                    }
+
+                }
+                else if (coef[i] == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (i == 0 && coef[i] == 1)
+                    {
+                        Console.Write("x");
+                    }
+                    else if (i == 0)
+                    {
+                        Console.Write("{0}x", coef[i]);
+                    }
+                    else
+                    {
+                        Console.Write("+{0}x", coef[i]);
+                    }
+                }
+            }
+
+        }
+        if (coef[coef.Length - 1]!= 0) {
+            if(coef[coef.Length-1] > 0)
+            Console.Write("+{0}",Coef[coef.Length - 1]);
+            else
+            {
+              Console.Write("{0}", Coef[coef.Length - 1]);
+            }
+        }
+    }
+
+    public double methodVal;
+    public void horner()
+    {
+        double[] hornerCoef = new double[coef.Length];
+        hornerCoef[0] = coef[0];
+        for (int i = 0, j = 1 ; i < hornerCoef.Length-1 && j<hornerCoef.Length; i++, j++)
+        {
+            hornerCoef[j] = hornerCoef[j-1] * methodVal + coef[i + 1];
+        }
+        if (hornerCoef[hornerCoef.Length-1] == 0)
+        {
+            Console.Write("{0} is a root to the equation: ",methodVal);
+            display();
+            Console.Write(" and the quotient is : ");
+            quotienPoly quot = new quotienPoly();
+            quot.Degree = Degree - 1;
+            for(int i = 0; i<=quot.Degree; i++)
+            {
+                quot.coef[i] = hornerCoef[i];
+            }
+            quot.display();
+        }
+        else
+        {
+            Console.Write("{0} is not a root to the equation: ", methodVal);
+                display();
+            Console.Write(" however the coefficients of the method are: ");
+        foreach(double element in hornerCoef)
+        {
+            Console.Write("{0} ",element);
+        }
+        }
+    }
+
+}
---
+++ Horner/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Horner
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Horner method");
+            static int degreeIn()
+            {
+                Console.Write("Insert the highest degree of the polynomial : ");
+                int n;
+                if (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+                {
+                    Console.WriteLine("Error you have entered an invalid degree");
+                    return degreeIn();
+                }
+                else
+                {
+                    return n;
+                }
+            } // gets the degree of the polynomial
+
+            
+            Polynomial poly1 = new Polynomial();
+            poly1.Degree = degreeIn();
+            int n = poly1.Degree;
+            for(int i = 0; i<=n; i++)
+            {
+                Console.Write("Insert {0} coefficient: ", i + 1);
+                     double coefIn()
+                {
+                    double coef;
+                    if(!double.TryParse(Console.ReadLine(), out coef)){
+                        Console.WriteLine("Error you have enetered an invalid value!");
+                        return coefIn();
+                    }
+                    else
+                    {
+                        return coef;
+                    }
+                }
+                poly1.coef[i] = coefIn();
+                
+            } // gets coeficients
+            Console.WriteLine("You have entered this polynomial:");
+            poly1.display(); // display polynomial
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continiue....");
+            Console.ReadKey();
+             void start()
+            {
+                 static double methodVal()
+                {
+                    double methodValue;
+                    Console.Write("Insert value for the method: ");
+                    if(!double.TryParse(Console.ReadLine(), out methodValue))
+                    {
+                        Console.WriteLine("You have entered an invalid value!");
+                        return methodVal();
+                    }
+                    else
+                    {
+                        return methodValue;
+                    }
+                    
+                }
+                poly1.methodVal = methodVal();
+                poly1.horner();
+                Console.ReadKey();
+                Console.WriteLine("Do you want to try again with same polynomial with another method value or do you wanna reset or close the prgoram?(T/R/C)");
+                string check = Console.ReadLine();
+                if (check == "T" || check == "t")
+                {
+                    start();
+                }
+                if (check == "R" || check == "r")
+                {
+                    Main();
+                }
+                    
+            }
+                start();
+            Console.WriteLine("Thank you for using me");
+            Console.WriteLine("Press any key to close the program.... bye : ) ");
+            Console.ReadKey();
+
+        }
+    }
+}
