320b1f2f374dd88d7c68dd09e5a871b77a448f65
Ilia Granchulov <iliya0620@outlook.com>
2024-12-08 15:46:27 +0200

    Made it more efficient

--- Horner/Program.cs
+++ Horner/Program.cs
@@ -1,88 +1,110 @@
 ﻿namespace Horner
 {
-    class Program
-    {
+    class Program { 
         public static void Main()
         {
-            Console.WriteLine("Horner method");
-            static int degreeIn()
+            bool isRunning = true;
+            while (isRunning)
             {
-                Console.Write("Insert the highest degree of the polynomial : ");
-                int n;
-                if (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+                Console.WriteLine("Horner method");
+                static int degreeIn()
                 {
-                    Console.WriteLine("Error you have entered an invalid degree");
-                    return degreeIn();
-                }
-                else
-                {
-                    return n;
-                }
-            } // gets the degree of the polynomial
+                    Console.Write("Insert the highest degree of the polynomial : ");
+                    int n;
+                    if (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+                    {
+                        Console.WriteLine("Error you have entered an invalid degree");
+                        return degreeIn();
+                    }
+                    else
+                    {
+                        return n;
+                    }
+                } // gets the degree of the polynomial
 
-            
-            Polynomial poly1 = new Polynomial();
-            poly1.Degree = degreeIn();
-            int n = poly1.Degree;
-            for(int i = 0; i<=n; i++)
-            {
-                Console.Write("Insert {0} coefficient: ", i + 1);
-                     double coefIn()
+
+                Polynomial poly1 = new Polynomial();
+                poly1.Degree = degreeIn();
+                int n = poly1.Degree;
+                for (int i = 0; i <= n; i++)
                 {
-                    double coef;
-                    if(!double.TryParse(Console.ReadLine(), out coef)){
-                        Console.WriteLine("Error you have enetered an invalid value!");
-                        return coefIn();
-                    }
-                    else
+                    Console.Write("Insert {0} coefficient: ", i + 1);
+                    double coefIn()
                     {
-                        return coef;
+                        double coef;
+                        if (!double.TryParse(Console.ReadLine(), out coef))
+                        {
+                            Console.WriteLine("Error you have enetered an invalid value!");
+                            return coefIn();
+                        }
+                        else
+                        {
+                            return coef;
+                        }
                     }
-                }
-                poly1.coef[i] = coefIn();
-                
-            } // gets coeficients
-            Console.WriteLine("You have entered this polynomial:");
-            poly1.display(); // display polynomial
-            Console.WriteLine();
-            Console.WriteLine("Press any key to continiue....");
-            Console.ReadKey();
-             void start()
-            {
-                 static double methodVal()
-                {
-                    double methodValue;
-                    Console.Write("Insert value for the method: ");
-                    if(!double.TryParse(Console.ReadLine(), out methodValue))
-                    {
-                        Console.WriteLine("You have entered an invalid value!");
-                        return methodVal();
-                    }
-                    else
-                    {
-                        return methodValue;
-                    }
-                    
-                }
-                poly1.methodVal = methodVal();
-                poly1.horner();
+                    poly1.coef[i] = coefIn();
+
+                } // gets coeficients
+                Console.WriteLine("You have entered this polynomial:");
+                poly1.display(); // display polynomial
+                Console.WriteLine();
+                Console.WriteLine("Press any key to continiue....");
                 Console.ReadKey();
-                Console.WriteLine("Do you want to try again with same polynomial with another method value or do you wanna reset or close the prgoram?(T/R/C)");
-                string check = Console.ReadLine();
-                if (check == "T" || check == "t")
+                void start()
                 {
-                    start();
+                    bool isStart = true;
+                    while(isStart)
+                    {
+                        static double methodVal()
+                        {
+                            double methodValue;
+                            Console.Write("Insert value for the method: ");
+                            if (!double.TryParse(Console.ReadLine(), out methodValue))
+                            {
+                                Console.WriteLine("You have entered an invalid value!");
+                                return methodVal();
+                            }
+                            else
+                            {
+                                return methodValue;
+                            }
+
+                        }
+                        poly1.methodVal = methodVal();
+                        poly1.horner();
+                        Console.ReadKey();
+                        Console.WriteLine("Do you want to try again with same polynomial with another method value or do you wanna reset or close the prgoram?(T/R/C)");
+                        string check = Console.ReadLine();
+                        switch (check)
+                        {
+                            default:
+                            case "C":
+                            case "c":
+                                isStart = false;
+                                isRunning = false;
+                                Console.WriteLine("Thank you for using me");
+                                Console.WriteLine("Press any key to close the program.... bye : ) ");
+                                Console.ReadKey();
+                                break;
+                            case "T":
+                            case "t":
+                                isStart = true;
+                                isRunning = false;
+                                break;
+                            case "R":
+                            case "r":
+                                isRunning = true;
+                                isStart = false;
+                                break;
+
+
+                        }
+                    }
+
                 }
-                if (check == "R" || check == "r")
-                {
-                    Main();
-                }
-                    
-            }
                 start();
-            Console.WriteLine("Thank you for using me");
-            Console.WriteLine("Press any key to close the program.... bye : ) ");
-            Console.ReadKey();
+                
+            }
 
         }
     }
